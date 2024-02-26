99e136d7bb244a53a99a35f8c61f561a1321e6ed
basavrajnjaliminche <basavrajnjaliminche@gmail.com>
2024-02-26 19:49:21 +0100

    operartors practice

---
+++ src/BasicsofJava/Operators.java
@@ -0,0 +1,20 @@
+package BasicsofJava;
+
+public class Operators {
+
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 7;
+        int c1 = a+b;
+        int c2 = a-b;
+        int c3= a*b;
+        int c4 = a/b;
+        int c5 = a%b;
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+        System.out.println(c5);
+    }
+}
